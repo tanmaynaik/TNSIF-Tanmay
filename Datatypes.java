054d0b0ffae5dbe3935321556955c7872f3dc0e7
tanmaynaik <68956673+tanmaynaik@users.noreply.github.com>
2024-07-18 21:38:43 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,12 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        // byte, short, int, long, float, double, boolean, char
+        int val1 = 12 / 6;
+        float val2 = 12f / 10f;
+        double val3 = 12 / 10;
+
+        System.out.println("Integer: " + val1);
+        System.out.println("Float: " + val2);
+        System.out.println("Double: " + val3);
+    }
+}
---
+++ ObjectSample.java
@@ -0,0 +1,11 @@
+class ExampleObject {
+    int a = 20;
+}
+
+
+public class ObjectSample {
+    public static void main(String[] args) {
+        ExampleObject obj = new ExampleObject();
+        System.out.println(obj.a);
+    }
+}
